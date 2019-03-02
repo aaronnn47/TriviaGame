776abc2c078ddc2259df9ba40cc36a5599eba9fc
byrdjess <byrdjess@me.com>
2019-03-02 00:59:03 -0700

    js6

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,33 @@
 $(document).ready(function() {
 
+var count = 10;
+
+var counter = 0;
+
 
   $("#quiz1").text("Press button to play!");
+  $("#quiz2").hide();
   
   
 
   $(".start").click( function(){
       $(this).hide();
       $("#quiz1").hide();
+      $("#quiz2").show();
+      counter = setInterval(timer, 1000);
 
     
     });
 
 
-    var count= 10;
-
-    var counter = setInterval(timer, 1000);
-
     function timer()
+    
     {
       count = count-1;
       if (count <= 0)
       {
         document.getElementById("timer").innerHTML= "Time's up!";
+        $("#quiz2").hide();
          clearInterval(counter);
          return;
       }
@@ -40,28 +45,32 @@ $(document).ready(function() {
         var question3 = document.quiz2.question3.value;
         var question4 = document.quiz2.question4.value;
         var correct = 0;
+        var incorrect = 0;
 
-        if (question1 == "a"){
+        if (question1 === "a"){
             correct++;
         }
-        if (question2 == "b"){
+        
+        if (question2 === "b"){
             correct++;
         }
-        if (question3 == "b"){
+
+        if (question3 === "b"){
             correct++;
         }
-        if (question4 == "a"){
+        
+        if (question4 === "a"){
             correct++;
         }
 
         $("#quiz2").hide();
-
         document.getElementById("number_correct").innerHTML = "You got" + correct + "/4" + "correct!";
 
 
     }
 
     check();
+    
 
 });
 
