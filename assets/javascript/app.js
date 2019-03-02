2b57b72148c49e0f8e6d93838cd61c4d6be352c4
byrdjess <byrdjess@me.com>
2019-03-02 01:18:41 -0700

    js7

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
 var count = 10;
-
+var correct = 0;
+var incorrect = 0;
 var counter = 0;
 
 
@@ -28,6 +29,8 @@ var counter = 0;
       {
         document.getElementById("timer").innerHTML= "Time's up!";
         $("#quiz2").hide();
+        document.getElementById("number_correct").innerHTML = "You got" + correct + "/4" + "correct!";
+        document.getElementById("number_incorrect").innerHTML = "You got" + incorrect + "/4" + "incorrect!";
          clearInterval(counter);
          return;
       }
@@ -44,27 +47,36 @@ var counter = 0;
         var question2 = document.quiz2.question2.value;
         var question3 = document.quiz2.question3.value;
         var question4 = document.quiz2.question4.value;
-        var correct = 0;
-        var incorrect = 0;
+
 
         if (question1 === "a"){
             correct++;
+        }else {
+            incorrect++;
         }
         
         if (question2 === "b"){
             correct++;
+        }else {
+            incorrect++;
         }
 
         if (question3 === "b"){
             correct++;
+        }else {
+            incorrect++;
         }
-        
+
         if (question4 === "a"){
             correct++;
+        }else {
+            incorrect++;
         }
 
         $("#quiz2").hide();
         document.getElementById("number_correct").innerHTML = "You got" + correct + "/4" + "correct!";
+        document.getElementById("number_incorrect").innerHTML = "You got" + incorrect + "/4" + "correct!";
+        clearInterval(counter);
 
 
     }
