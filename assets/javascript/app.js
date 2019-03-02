ae644cf92494e7316a07a76f09cc545501334e77
byrdjess <byrdjess@me.com>
2019-03-01 23:22:49 -0700

    js3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,15 +3,34 @@ $(document).ready(function() {
 
   $("#quiz1").text("Press button to play!");
   $("#quiz2").hide();
+  
 
   $(".start").click( function(){
       $(this).hide();
-      $("#quiz1").hide();     
-      
+      $("#quiz1").hide();
 
+    
     });
 
-      
+
+    var count= 10;
+
+    var counter = setInterval(timer, 1000);
+
+    function timer()
+    {
+      count = count-1;
+      if (count <= 0)
+      {
+        document.getElementById("timer").innerHTML= "Time's up!";
+         clearInterval(counter);
+         return;
+      }
+    
+     document.getElementById("timer").innerHTML= count + " secs"; 
+    }
 
 
+
+    
 });
