773a0813c60c2af2fdb59046dde20423b8f10219
byrdjess <byrdjess@me.com>
2019-03-01 21:09:02 -0700

    js1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,47 @@
 $(document).ready(function() {
 
+ var myQuestions = {
+     questions: [
+         {q: "What is this?",
+         choices: [
+             "a", "b", "c"],
+         correctAnswer: "b"
+        }
+     ],
+     questions: [
+        {q: "What is this?",
+        choices: [
+            "a", "b", "c"],
+        correctAnswer: "c"
+       }
+    ],
+    questions: [
+        {q: "What is this?",
+        choices: [
+            "a", "b", "c"],
+        correctAnswer: "a"
+       }
+    ],
 
+ }
 
+  $("#quiz").text("Press button to play!");
 
+  $(".start").click( function(){
+      $(this).hide();
+      $("#quiz").hide();
+      for(var i = 0; i < myQuestions.length; i++){
 
+        $("#questions").text(myQuestions.questions.q);
+        console.log(myQuestions.questions.q);
+s
 
-    $("header").click(function(){
-
-        $("div").animate({
-          left: '350px',
-          width: '150px'
-        }); 
-
+      }
+      
 
     });
 
-
-
-
+      
 
 
 });
