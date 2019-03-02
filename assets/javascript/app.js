aac16355ec96615007101cbd3cbf5bde9235db4f
byrdjess <byrdjess@me.com>
2019-03-01 21:50:26 -0700

    js2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,12 @@
 $(document).ready(function() {
 
- var myQuestions = {
-     questions: [
-         {q: "What is this?",
-         choices: [
-             "a", "b", "c"],
-         correctAnswer: "b"
-        }
-     ],
-     questions: [
-        {q: "What is this?",
-        choices: [
-            "a", "b", "c"],
-        correctAnswer: "c"
-       }
-    ],
-    questions: [
-        {q: "What is this?",
-        choices: [
-            "a", "b", "c"],
-        correctAnswer: "a"
-       }
-    ],
 
- }
-
-  $("#quiz").text("Press button to play!");
+  $("#quiz1").text("Press button to play!");
+  $("#quiz2").hide();
 
   $(".start").click( function(){
       $(this).hide();
-      $("#quiz").hide();
-      for(var i = 0; i < myQuestions.length; i++){
-
-        $("#questions").text(myQuestions.questions.q);
-        console.log(myQuestions.questions.q);
-s
-
-      }
+      $("#quiz1").hide();     
       
 
     });
